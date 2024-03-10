9d02550b1eed06136b0682fc9897b7041645c3c4
Amber Williams <amberwilliamsdev@gmail.com>
2024-03-10 15:47:18 +0000

    feat: utility script to convert sqlite data into CSV files for postgres use

---
+++ scripts/convert-sqlite-to-csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect("./../api/src/ricotta/services/development.db", isolation_level=None,
+                       detect_types=sqlite3.PARSE_COLNAMES)
+db_df = pd.read_sql_query("SELECT * FROM cards", conn)
+db_df.to_csv('cards.csv', index=False)
+db_df = pd.read_sql_query("SELECT * FROM incorrect_options", conn)
+db_df.to_csv('incorrect_options.csv', index=False)
